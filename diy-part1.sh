b58340dbfeb8d347cd268839dba69a31f5c3773c
anonymous <50570049+xsj684135@users.noreply.github.com>
2024-04-19 18:24:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git packages https://git.openwrt.org/feed/packages.git' >>feeds.conf.default
-echo 'src-git luci https://git.openwrt.org/project/luci.git'>>feeds.conf.default
-echo 'src-git routing https://git.openwrt.org/feed/routing.git'>>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git'>>feeds.conf.default
+echo 'src-git packages https://git.openwrt.org/feed/packages.git^063b2393cbc3e5aab9d2b40b2911cab1c3967c59' >>feeds.conf.default
+echo 'src-git luci https://git.openwrt.org/project/luci.git^b07cf9dcfc37e021e5619a41c847e63afbd5d34a'>>feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git^648753932d5a7deff7f2bdb33c000018a709ad84'>>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git^86af194d03592121f5321474ec9918dd109d3057'>>feeds.conf.default
